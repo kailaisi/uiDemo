dd527c2eb1260bae0a30fa9549d36143f216273a
wujinxiang <541018378@qq.com>
2021-04-23 17:24:03 +0800

    init

---
+++ app/src/main/java/com/kailaisi/uidemo/DreamEfectView.java
@@ -0,0 +1,76 @@
+package com.kailaisi.uidemo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 通过使用Shade来将图片进行一个变换处理
+ * 1。先通过ColorMitra来进行绿色处理
+ * 2。
+ */
+public class DreamEfectView extends View {
+    private Paint mPaint,mShaderPaint;
+    private PorterDuffXfermode mode;
+
+    private Bitmap mBitmmap;
+    private int x, y;
+
+    public DreamEfectView(Context context) {
+        super(context);
+    }
+
+    public DreamEfectView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DreamEfectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        //生成Bitmap
+        mBitmmap = BitmapFactory.decodeResource(getResources(), R.mipmap.f);
+        int[] screenSize = ScreenUtil.getScreenSize(context);
+        //设置模式为源在内部
+        mode=new PorterDuffXfermode(PorterDuff.Mode.SCREEN);
+        //图像中心
+        x=0;//screenSize[0]/2-mSrcBitmap.getWidth()/2;
+        y=0;//screenSize[1]/2-mSrcBitmap.getHeight()/2;
+        mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        // 去饱和、提亮、色相矫正
+        mPaint.setColorFilter(new ColorMatrixColorFilter(new float[] { 0.8587F, 0.2940F, -0.0927F, 0, 6.79F, 0.0821F, 0.9145F, 0.0634F, 0, 6.79F, 0.2019F, 0.1097F, 0.7483F, 0, 6.79F, 0, 0, 0, 1, 0 }));
+
+        mShaderPaint=new Paint();
+        //设置径向渐变，中心是图片的中心，
+        //颜色中心为透明，两边为黑色
+        mShaderPaint.setShader(new RadialGradient(mBitmmap.getWidth()/2,mBitmmap.getHeight()/2,mBitmmap.getHeight(),
+                Color.TRANSPARENT,Color.BLACK, Shader.TileMode.CLAMP));
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+       canvas.drawColor(Color.BLACK);
+        int layer = canvas.saveLayer(x, y, x + mBitmmap.getWidth(), y + mBitmmap.getHeight(),null,Canvas.ALL_SAVE_FLAG);
+        canvas.drawColor(0xcc1c093e);
+        mPaint.setXfermode(mode);
+        canvas.drawBitmap(mBitmmap,x,y,mPaint);
+        mPaint.setXfermode(null);
+        canvas.restoreToCount(layer);
+        //绘制一个矩形。中间透明，两边黑色，突出图片
+        canvas.drawRect(x,y,x+mBitmmap.getWidth(),mBitmmap.getHeight(),mShaderPaint);
+    }
+}
--- app/src/main/java/com/kailaisi/uidemo/J.java
+++ app/src/main/java/com/kailaisi/uidemo/J.java
@@ -14,48 +14,47 @@ import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 
-import androidx.annotation.Nullable;
+public class J extends View {
+    private Bitmap mSrcBitmap, mRefBitmap;// 位图
+    private Paint mPaint;// 画笔
+    private PorterDuffXfermode mXfermode;// 混合模式
 
-/**
- * 一个倒影效果的美女
- */
-public class ReflectView extends View {
-    private Paint mPaint;
-    private Bitmap mSrcBitmap, mRefBitmap;
-    private PorterDuffXfermode mode;
+    private int x, y;// 位图起点坐标
 
-    private int x, y;
+    public J(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-    public ReflectView(Context context) {
-        super(context);
+        // 初始化资源
+        initRes(context);
     }
 
-    public ReflectView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public ReflectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-
-        //生成Bitmap
+    /*
+     * 初始化资源
+     */
+    private void initRes(Context context) {
+        // 获取源图
         mSrcBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.a);
-        Matrix matrix = new Matrix();
-        //设置倒影效果，y轴取反，x轴不变
-        matrix.setScale(1F,-1F);
-        //创建原图像的倒影图像
-        mRefBitmap =Bitmap.createBitmap(mSrcBitmap,0,0,mSrcBitmap.getWidth(),mSrcBitmap.getHeight(),matrix,true);
-        int[] screenSize = ScreenUtil.getScreenSize(context);
-        //图像中心
-        x=0;//screenSize[0]/2-mSrcBitmap.getWidth()/2;
-        y=0;//screenSize[1]/2-mSrcBitmap.getHeight()/2;
-        mPaint=new Paint();
-        //设置从中间位置开始向下，进行颜色渐变，从灰到透明
-        LinearGradient shader = new LinearGradient(x, y + mSrcBitmap.getHeight(), x, y + mSrcBitmap.getHeight() + mSrcBitmap.getHeight() / 4, 0xAA000000,Color.TRANSPARENT, Shader.TileMode.CLAMP);
-        mPaint.setShader(shader);
-        //设置模式为源在内部。这时候是黑色透明，白色不透明
-        mode=new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
-    }
 
+        // 实例化一个矩阵对象
+        Matrix matrix = new Matrix();
+        matrix.setScale(1F, -1F);
+
+        // 生成倒影图
+        mRefBitmap = Bitmap.createBitmap(mSrcBitmap, 0, 0, mSrcBitmap.getWidth(), mSrcBitmap.getHeight(), matrix, true);
+
+        int screenH = 0;
+
+        int screenW=0;
+        x = screenW / 2 - mSrcBitmap.getWidth() / 2;
+        y = screenH / 2 - mSrcBitmap.getHeight() / 2;
+
+        // ………………………………
+        mPaint = new Paint();
+        mPaint.setShader(new LinearGradient(x, y + mSrcBitmap.getHeight(), x, y + mSrcBitmap.getHeight() + mSrcBitmap.getHeight() / 4, 0xAA000000, Color.TRANSPARENT, Shader.TileMode.CLAMP));
+
+        // ………………………………
+        mXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -67,7 +66,7 @@ public class ReflectView extends View {
         int sc = canvas.saveLayer(x, y + mSrcBitmap.getHeight(), x + mRefBitmap.getWidth(), x + mSrcBitmap.getHeight() * 2, null, Canvas.ALL_SAVE_FLAG);
         //画倒影，倒影的位置，倒影的起始位置，是从原图的底部开始的
         canvas.drawBitmap(mRefBitmap, x, y + mSrcBitmap.getHeight(), null);
-        mPaint.setXfermode(mode);
+        mPaint.setXfermode(mXfermode);
         //画渐变的方形，位置和倒影是一样的。但是使用了mode。所以底部是白色的，然后sr效果就是没有了。。。
         canvas.drawRect(x, y + mSrcBitmap.getHeight(), x + mRefBitmap.getWidth(), y + mSrcBitmap.getHeight() * 2, mPaint);
         mPaint.setXfermode(null);
